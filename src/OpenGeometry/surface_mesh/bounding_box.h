ed8b1d5cd905181d68e9adca8ce72697625e8f65
Andrea Tagliasacchi <andrea.tagliasacchi@gmail.com>
2014-02-05 18:11:27 +0100

    bounding box + operator<<

--- src/OpenGeometry/surface_mesh/bounding_box.h
+++ src/OpenGeometry/surface_mesh/bounding_box.h
@@ -1,17 +1,31 @@
 #include <Eigen/Geometry>
 #include <surface_mesh/Surface_mesh.h>
+
+namespace Eigen{
+
+template <typename Scalar,int _AmbientDim>
+inline std::ostream& operator<<(std::ostream& os, const Eigen::AlignedBox<Scalar,_AmbientDim>& box)
+{
+    os << "[min[" << box.min().transpose() << "] max[" << box.max().transpose() << "]]";
+    return os;
+}
+
+} // Eigen
+
+
 namespace surface_mesh{
 
-typedef Eigen::AlignedBox<Scalar,3> Box3;
-Box3 bounding_box(const Surface_mesh& mesh){
+/// @todo should we move this
+typedef Eigen::AlignedBox<Scalar,3> Box3; 
+Box3 bounding_box(const Surface_mesh& mesh)
+{
     auto vpoints = mesh.get_vertex_property<Vec3>("v:point");
     Box3 bbox;
     bbox.setNull();
-    for(Surface_mesh::Vertex v: mesh.vertices()){
-        Vec3f vec;   
-        bbox.extend( vec /*vpoints[v]*/ );
-    }
-    return bbox;    
+    for(auto v: mesh.vertices())
+        bbox.extend( vpoints[v] );
+    return bbox;
 }
-    
+
+   
 } // surface_mesh
