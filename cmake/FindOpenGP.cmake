5fa4c740d4e5b3272b71d6495d86dfdbbc4980a2
Andrea Tagliasacchi <andrea.tagliasacchi@gmail.com>
2014-02-13 16:10:14 +0100

    [cmake] updated premade cmake FindPackage

--- cmake/FindOpenGP.cmake
+++ cmake/FindOpenGP.cmake
@@ -1,20 +1,19 @@
 # Locate the OpenGP library (version 1.0)
 # This module defines the following variables:
 # OpenGP_INCLUDE_DIRS, where to find OpenGP include files.
-# OpenGP_FOUND, true if library path was resolved
+# OpenGP_FOUND and OPENGP_FOUND true if library path was resolved
 #
 # Usage example to compile an "executable" target:
 #
 # FIND_PACKAGE (OpenGP REQUIRED)
 # INCLUDE_DIRECTORIES (${OpenGP_INCLUDE_DIRS})
 # ADD_EXECUTABLE (executable ${YOUR_EXECUTABLE_SRCS})
-#
-
+# 
 # DEBUG: outputs given environment variable
-#message(STATUS OpenGPDIR $ENV{OpenGPDIR})
+# message(STATUS OpenGPDIR $ENV{OpenGP_DIR})
 
-FIND_PATH( OpenGP_INCLUDE_DIRS OpenGP/Surface_mesh/Surface_mesh.h
-    $ENV{OpenGPDIR}
+FIND_PATH( OpenGP_INCLUDE_DIRS OpenGP/Surface_mesh.h
+    $ENV{OpenGP_DIR}
     /usr/local/include
     /usr/local/X11R6/include
     /usr/X11R6/include
@@ -24,10 +23,9 @@ FIND_PATH( OpenGP_INCLUDE_DIRS OpenGP/Surface_mesh/Surface_mesh.h
     /opt/X11/include
     /opt/include)
 
-# @todo this might be done in a better way in the FIND_PATH above
-SET(OpenGP_INCLUDE_DIRS ${OpenGP_INCLUDE_DIRS}/OpenGP)
-
+SET(OPENGP_FOUND "NO")
 SET(OpenGP_FOUND "NO")
 IF(OpenGP_INCLUDE_DIRS)
-  SET(OpenGP_FOUND "YES")
-ENDIF(OpenGP_INCLUDE_DIRS)
+    SET(OPENGP_FOUND "YES")
+    SET(OpenGP_FOUND "YES")
+ENDIF()
