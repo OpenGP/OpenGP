f2714514e7c171c8c0d86a7fe626fb932a9dbeb4
Andrea Tagliasacchi <andrea.tagliasacchi@gmail.com>
2015-12-31 22:38:39 -0700

    [minor fixes] glfw viewer

--- apps/glfwviewer/vshader.glsl
+++ apps/glfwviewer/vshader.glsl
@@ -16,15 +16,14 @@ void main(){
     gl_Position = MVP * vec4(vposition, 1.0); /// Clip coordinates
     
     ///--- Normal in world coordinates
-    // @see https://code.google.com/p/opengl-tutorial-org/source/browse/tutorial09_vbo_indexing/StandardShading.vertexshader
-    // Only correct if ModelMatrix does not scale the model ! Use its inverse transpose if not.
+    /// TODO: inverse transpose
     vec4 n = M * vec4(vnormal, 0);
     
-    ///--- Normalization
+    ///--- Normalizations
     n.xyz = normalize(n.xyz);
     vec3 ldir = normalize(LDIR);
     
-    ///--- per vertex shading
+    ///--- (Gouraud) per-vertex shading 
     vec3 basecolor = vec3(1,0,0);
     float albedo = max( dot( n.xyz, ldir ), 0 );   
     vColor = vec4(basecolor * albedo,1);
