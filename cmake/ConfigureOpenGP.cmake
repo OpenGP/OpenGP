6826ea61cbb9263d0486cbc165684965e8562791
Andrea Tagliasacchi <andrea.tagliasacchi@gmail.com>
2015-12-31 23:20:44 -0700

    [cmake] fixed "duplicate target" problem when using self-contained apps

--- cmake/ConfigureOpenGP.cmake
+++ cmake/ConfigureOpenGP.cmake
@@ -3,7 +3,10 @@ find_package(OpenGP REQUIRED)
 include_directories(${OpenGP_INCLUDE_DIR})
 #message(STATUS ${OpenGP_INCLUDE_DIR})
 
-#--- Dummy target just to have sources/headers appear in IDE
-file(GLOB_RECURSE SOURCES "${OpenGP_INCLUDE_DIR}/*.cpp")
-file(GLOB_RECURSE HEADERS "${OpenGP_INCLUDE_DIR}/*.h")
-add_custom_target(OPENGP_TARGET SOURCES ${HEADERS} ${SOURCES})
+#< avoid errors caused by duplicates
+if(NOT TARGET OPENGP) 
+    #--- Dummy target just to have sources/headers appear in IDE
+    file(GLOB_RECURSE SOURCES "${OpenGP_INCLUDE_DIR}/*.cpp")
+    file(GLOB_RECURSE HEADERS "${OpenGP_INCLUDE_DIR}/*.h")
+    add_custom_target(OPENGP SOURCES ${HEADERS} ${SOURCES})
+endif()
