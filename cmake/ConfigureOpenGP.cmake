4ea0c87156b731f5d976649bfd1c890336319ffc
Andrea Tagliasacchi <andrea.tagliasacchi@gmail.com>
2015-12-31 23:07:31 -0700

    ConfigureOpenGP now creates a custom dummy for OpenGP sources/headers

--- cmake/ConfigureOpenGP.cmake
+++ cmake/ConfigureOpenGP.cmake
@@ -1,3 +1,9 @@
 #--- OpenGP (headeronly)
 find_package(OpenGP REQUIRED)
 include_directories(${OpenGP_INCLUDE_DIR})
+#message(STATUS ${OpenGP_INCLUDE_DIR})
+
+#--- Dummy target just to have sources/headers appear in IDE
+file(GLOB_RECURSE SOURCES "${OpenGP_INCLUDE_DIR}/*.cpp")
+file(GLOB_RECURSE HEADERS "${OpenGP_INCLUDE_DIR}/*.h")
+add_custom_target(OPENGP_TARGET SOURCES ${HEADERS} ${SOURCES})
