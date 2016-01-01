038c4d17e99873550febf0bc29ac53db8e00edd0
Andrea Tagliasacchi <andrea.tagliasacchi@gmail.com>
2015-12-31 23:34:41 -0700

    [cmake] Configure*.cmake now using ${LIBRARIES}

--- cmake/ConfigureGLEW.cmake
+++ cmake/ConfigureGLEW.cmake
@@ -2,3 +2,4 @@
 find_package(GLEW REQUIRED)
 include_directories(${GLEW_INCLUDE_DIRS})
 link_directories(${GLEW_LIBRARY_DIRS})
+list(APPEND LIBRARIES ${GLEW_LIBRARIES})
--- cmake/ConfigureGLFW.cmake
+++ cmake/ConfigureGLFW.cmake
@@ -1,7 +1,8 @@
-#--- Context manager
+#--- OpenGL Context Manager
 find_package(GLFW REQUIRED)
 include_directories(${GLFW_INCLUDE_DIRS})
 add_definitions(${GLFW_DEFINITIONS})
+list(APPEND LIBRARIES ${GLFW_LIBRARIES})
 
 if(NOT GLFW_FOUND)
     message(WARNING "GLFW not found")
