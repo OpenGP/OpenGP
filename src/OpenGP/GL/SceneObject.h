24ecfe356c04f8f72b9463991f069f8fe845a473
Andrea Tagliasacchi <andrea.tagliasacchi@gmail.com>
2016-01-15 06:06:42 -0800

    [GL] objects have a default per-vertex color

--- src/OpenGP/GL/SceneObject.h
+++ src/OpenGP/GL/SceneObject.h
@@ -14,10 +14,13 @@ namespace OpenGP {
 //=============================================================================
 
 class SceneObject{
+// TODO: split public/private
 public:
     ShaderProgram program;
     /// Model Transformation Matrix
     Mat4x4 model = Mat4x4::Identity();
+    /// Per-object color
+    Vec3 color = Vec3(.6,.6,.6);
 
     virtual ~SceneObject(){}
     virtual void init() = 0;
