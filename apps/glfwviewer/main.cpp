240cdb9b87f96d0f4d711463b6f752bfcd80e762
Andrea Tagliasacchi <andrea.tagliasacchi@gmail.com>
2016-01-16 14:01:18 -0800

    [apps/glfwviewer] added point cloud w/ normals renderer

--- apps/glfwviewer/main.cpp
+++ apps/glfwviewer/main.cpp
@@ -6,13 +6,10 @@
 #include <OpenGP/SurfaceMesh/SurfaceMesh.h>
 #include <OpenGP/SurfaceMesh/GL/SurfaceMeshRenderFlat.h>
 #include <OpenGP/SurfaceMesh/GL/SurfaceMeshRenderShaded.h>
-
-///--- Point clouds
-#include <OpenGP/MATLAB/random.h>
-#include <OpenGP/GL/PointCloudRenderer.h>
+#include <OpenGP/SurfaceMesh/GL/SurfaceMeshRenderCloud.h>
+#include <OpenGP/SurfaceMesh/GL/SurfaceMeshRenderVertexNormals.h>
 
 using namespace OpenGP;
-
 int main(int argc, char** argv){   
     if(argc!=2) mFatal("usage: glfwviewer bunny.obj");
     
@@ -23,26 +20,25 @@ int main(int argc, char** argv){
     mesh.update_face_normals();
     mesh.update_vertex_normals();
 
-    GlfwWindow window("glfwviewer",640,480);
+    GlfwWindow window("glfwviewer",1000,300);
 
+    ///--- Smooth shading
     SurfaceMeshRenderShaded shaded(mesh);
-    shaded.model = OpenGP::translate(-.5,0,0);
+    shaded.model = translate(-.6,0,0) * scale(.5);
     window.scene.add(shaded);
     
+    ///--- Flat shading (with wiremesh)
     SurfaceMeshRenderFlat flat(mesh);
-    flat.model = OpenGP::translate(+.5,0,0);
+    flat.model = translate(+.6,0,0) * scale(.5);
     window.scene.add(flat);
-    
-    
-    ///--- Renders some colored points 
-    int n = 100;
-    MatMxN P = .3f * OpenGP::randn(3,n);
-    MatMxN C = OpenGP::rand(3,n);
-    PointCloudRenderer rend(P);
-    rend.model = OpenGP::translate(0,+.5,0);
-    window.scene.add(rend);
-    rend.set_vsize(15);
-    rend.set_colors(C);
+        
+    ///--- Point cloud w/ normals
+    SurfaceMeshRenderCloud cloud(mesh);
+    SurfaceMeshRenderVertexNormals vnormals(mesh, .05);
+    cloud.model = scale(.5);
+    vnormals.model = scale(.5);
+    window.scene.add(cloud);
+    window.scene.add(vnormals);
     
     return window.run();
 }
