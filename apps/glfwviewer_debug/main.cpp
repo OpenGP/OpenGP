c2e5b32d028ee1b94fed6d196565c9a5add7012a
Andrea Tagliasacchi <andrea.tagliasacchi@gmail.com>
2016-01-31 00:13:29 -0800

    [new] apps/glfwviewer_debug

---
+++ apps/glfwviewer_debug/main.cpp
@@ -0,0 +1,30 @@
+#include <OpenGP/types.h>
+#include <OpenGP/MLogger.h>
+#include <OpenGP/GL/GlfwWindow.h>
+#include <OpenGP/MATLAB/random.h>
+#include <OpenGP/GL/PointsRenderer.h>
+#include <OpenGP/GL/SegmentsRenderer.h>
+
+using namespace OpenGP;
+int main(int, char**){    
+    GlfwWindow window("debugging in 3D",640,480);
+
+    int num_points = 500;
+    
+    ///--- Colored points rendering
+    MatMxN P = .3f*OpenGP::randn(3,num_points);
+    PointsRenderer cloud(P);
+    window.scene.add(cloud);
+    cloud.set_colors( OpenGP::rand(3, P.cols()) );
+    cloud.set_vsize(10);
+    
+    ///--- Line segments renderering
+    MatMxN Z = MatMxN::Zero(3,num_points);
+    SegmentsRenderer segments(Z,P);
+    segments.color = Vec3(1,0,0);
+    window.scene.add(segments);
+    
+    return window.run();
+}
+
+
