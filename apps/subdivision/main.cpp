a0a80564fd6ec351031d16cb3c4665a3e3a310f0
Andrea Tagliasacchi <andrea.tagliasacchi@gmail.com>
2016-02-12 20:16:15 -0400

    [new] app/subdivision

---
+++ apps/subdivision/main.cpp
@@ -0,0 +1,32 @@
+#include <OpenGP/SurfaceMesh/GL/SurfaceMeshRenderShaded.h>
+#include <OpenGP/SurfaceMesh/GL/SurfaceMeshRenderFlat.h>
+#include <OpenGP/SurfaceMesh/Subdivision/Loop.h>
+#include <OpenGP/GL/GlfwWindow.h>
+using namespace OpenGP;
+
+struct MainWindow : public GlfwWindow{
+    SurfaceMesh mesh;
+    SurfaceMeshRenderFlat renderer = SurfaceMeshRenderFlat(mesh);
+ 
+    MainWindow(int argc, char** argv) : GlfwWindow(__FILE__,400,400){
+        if(argc!=2) mFatal("application requires one parameter! e.g. sphere.obj");
+        bool success = mesh.read(argv[1]);
+        if(!success) mFatal() << "File not found: " << argv[1];
+        mesh.update_face_normals(); ///< shading
+        this->scene.add(renderer);
+    }
+    
+    void key_callback(int key, int scancode, int action, int mods) override{
+        GlfwWindow::key_callback(key, scancode, action, mods);
+        if(key==GLFW_KEY_SPACE && action==GLFW_RELEASE){
+            SurfaceMeshSubdivideLoop::exec(mesh);
+            mesh.update_face_normals();
+            renderer.init_data();
+        }
+    }
+};
+
+int main(int argc, char** argv){
+    MainWindow window(argc, argv);
+    return window.run();
+}
