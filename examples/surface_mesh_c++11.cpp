2329a3fbfc542d947a962b249bb23649ba4b49f0
Andrea Tagliasacchi <andrea.tagliasacchi@gmail.com>
2016-04-03 04:57:09 -0700

    [examples] parsed with astyle

--- examples/surface_mesh_c++11.cpp
+++ examples/surface_mesh_c++11.cpp
@@ -1,21 +1,21 @@
 /// Barycenter example re-written with C++11 syntax
-
 #include <OpenGP/SurfaceMesh/SurfaceMesh.h>
+#include <OpenGP/MLogger.h>
 using namespace OpenGP;
 using namespace std;
 
-int main(int argc, char** argv){
+int main(int argc, char** argv) {
+    std::string file = (argc>1) ? argv[1] : "bunny.obj";
+
     SurfaceMesh mesh;
-    if(!(argc==2 && mesh.read(argv[1]))){ 
-        cout << "!!! file doesn't exist or no file provided" << endl;
-        exit(0);
-    }
-    
+    bool success = mesh.read(file);
+    CHECK(success);
+
     auto points = mesh.get_vertex_property<Vec3>("v:point");
     Vec3 p(0,0,0);
-    for(auto vit: mesh.vertices())
+    for (auto vit: mesh.vertices())
         p += points[vit];
     p /= mesh.n_vertices();
-    
+
     std::cout << "barycenter: " << p << std::endl;
 }
--- examples/surface_mesh_io.cpp
+++ examples/surface_mesh_io.cpp
@@ -1,20 +1,26 @@
 #include <OpenGP/SurfaceMesh/SurfaceMesh.h>
+#include <OpenGP/MLogger.h>
 using namespace OpenGP;
 
-int main(int /*argc*/, char** argv)
-{
+int main(int argc, char** argv) {
+    std::string in_file = (argc>1) ? argv[1] : "bunny.obj";
+    std::string out_file = (argc>2) ? argv[2] : "output.obj";
+
     // instantiate a SurfaceMesh object
     SurfaceMesh mesh;
 
     // read a mesh specified as the first command line argument
-    mesh.read(argv[1]);
+    bool success = mesh.read(in_file);
+    CHECK(success);
 
     // ...
     // do fancy stuff with the mesh
     // ...
 
     // write the mesh to the file specified as second argument
-    mesh.write(argv[2]);
+    mesh.write(out_file);
+    CHECK(success);
 
-    return 0;
+    mLogger() << "read:" << in_file << "wrote:" << out_file;
+    return EXIT_SUCCESS;
 }
