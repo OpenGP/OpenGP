9e8eb682879384644a702db31bdb6f2b77c0cc96
Andrea Tagliasacchi <andrea.tagliasacchi@gmail.com>
2016-04-03 05:02:49 -0700

    [cmake] improved HEADERONLY mechanism

--- cmake/ConfigureOpenGP.cmake
+++ cmake/ConfigureOpenGP.cmake
@@ -1,12 +1,20 @@
-#--- OpenGP (headeronly)
 find_package(OpenGP REQUIRED)
 include_directories(${OpenGP_INCLUDE_DIR})
 #message(STATUS ${OpenGP_INCLUDE_DIR})
 
-#< avoid errors caused by duplicates
+#--- avoid errors caused by duplicates
 if(NOT TARGET OPENGP) 
-    #--- Dummy target just to have sources/headers appear in IDE
+    # Dummy target just to have sources/headers appear in IDE
     file(GLOB_RECURSE SOURCES "${OpenGP_INCLUDE_DIR}/*.cpp")
     file(GLOB_RECURSE HEADERS "${OpenGP_INCLUDE_DIR}/*.h")
     add_custom_target(OPENGP SOURCES ${HEADERS} ${SOURCES})
 endif()
+
+#--- Uncomment this SET to disable headeronly
+set(OPENGP_HEADERONLY TRUE)
+if(OPENGP_HEADERONLY)
+    add_definitions(-DOPENGP_HEADERONLY)   
+else()
+    list(APPEND LIBRARIES OpenGP)    
+endif()
+
