bade8ff77c0eff19ac44125d3b0481149d0e1efe
Andrea Tagliasacchi <andrea.tagliasacchi@gmail.com>
2016-04-05 16:44:05 -0700

    [cmake] Compiler defaulted to clang on mac (now supports C++11)

--- cmake/ConfigureCompiler.cmake
+++ cmake/ConfigureCompiler.cmake
@@ -5,23 +5,26 @@ if(NOT CMAKE_BUILD_TYPE)
 endif()
 
 #--- Add debug symbols if we are building in debug mode
-if(${CMAKE_BUILD_TYPE} STREQUAL "Debug")
+if(UNIX AND ${CMAKE_BUILD_TYPE} STREQUAL "Debug")
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -ggdb")
 endif()
 
-#--- set default compiler flags
-#if(UNIX AND CMAKE_COMPILER_IS_GNUCXX)
-#    message(STATUS "Using gcc/g++ compiler")
-#    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++0x -pedantic -Wextra -Wno-long-long")
-#endif()
-#if("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
-#    message(STATUS "Using Clang compiler")
-#    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11 -pedantic -Wextra -Wno-long-long")
-#endif()
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++0x -pedantic -Wextra -Wno-long-long")
+#--- UBUNTU
+if(UNIX AND NOT APPLE AND CMAKE_COMPILER_IS_GNUCXX)
+    message(STATUS "Using gcc/g++ compiler (Linux)")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++0x -pedantic -Wextra -Wno-long-long")
+endif()
 
-#--- windows fixes
+#--- APPLE 
+if(APPLE AND "${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
+    message(STATUS "Using Clang compiler on (Apple)")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11 -pedantic -Wextra -Wno-long-long")
+endif()
+
+#--- WINDOWS
 if(WIN32)
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -D_USE_MATH_DEFINES")
 endif()
 
+#--- OVERRIDE WITH ANYTHING YOU WANT
+# set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++0x -pedantic -Wextra -Wno-long-long")
